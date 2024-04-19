252333514e8a06bfca39c9a3ac17d78d7bffe16e
yyxi <178086254+yyxi@users.noreply.github.com>
2024-04-19 11:25:25 +0000

    chore: eslint

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
+  import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
 }
