c339644df39bacf0f97a43c1457d9dd8627e8577
yyxi <178086254+yyxi@users.noreply.github.com>
2024-11-19 12:52:23 +0000

    feat: refactor

--- src/index.ts
+++ src/index.ts
@@ -31,8 +31,11 @@ interface ActionFrameEnd {
 }
 
 export type Action = ActionFrameBegin | ActionFrameDraw | ActionFrameEnd | ActionFrameUpdate
-
-export type Unsubscribe = () => void
+export interface SubscriptionControls {
+  pause: () => void
+  resume: () => void
+  unsubscribe: () => void
+}
 export type Subscription = (action: Action) => void
 
 interface Options {
@@ -42,55 +45,104 @@ interface Options {
   timestep: number
 }
 
-// Four seconds
-const calculateMaxUpdateSteps = (timestep: number) => Math.round(4000 / timestep)
+export interface SubscriptionState {
+  active: boolean
+  value: Subscription
+}
 
-const createState = (options: Partial<Pick<Options, 'fps' | 'timestep'>>) => {
+const DEFAULT_TIMESTEP = 1000 / 60
+
+const isPositiveNumber = (input: unknown): input is number =>
+  typeof input === 'number' && input > 0 && Number.isFinite(input)
+
+const isUndefined = (input: unknown): input is undefined => input === undefined
+
+const isPositiveNumberOrUndefined = (input: unknown): input is number | undefined =>
+  isPositiveNumber(input) || isUndefined(input)
+
+function assert(condition: boolean, message: string): asserts condition {
+  if (!condition) {
+    throw new Error(message)
+  }
+}
+function assertIsFPS(input: unknown): asserts input is number | undefined {
+  assert(isPositiveNumberOrUndefined(input), '[seedpods] fps must be a positive number')
+}
+function assertIsTimestep(input: unknown): asserts input is number | undefined {
+  assert(isPositiveNumberOrUndefined(input), '[seedpods] timestep must be a positive number')
+}
+
+function assertOptions(
+  options: Partial<Pick<Options, 'fps' | 'timestep'>>,
+): asserts options is Partial<Pick<Options, 'fps' | 'timestep'>> {
+  assertIsFPS(options.fps)
+  assertIsTimestep(options.timestep)
+}
+
+const subscriptionRemove = (
+  subscription: Subscription,
+  subscriptions: SubscriptionState[],
+): SubscriptionState[] => subscriptions.filter((element) => element.value !== subscription)
+
+const subscriptionFind = (subscription: Subscription, subscriptions: SubscriptionState[]) =>
+  subscriptions.find((element) => element.value === subscription)
+
+const subscriptionActive = (subscriptions: SubscriptionState[]) =>
+  subscriptions.some((element) => element.active)
+
+const enum TypeState {
+  Active = 0,
+  Idle = 1,
+  Paused = 2,
+}
+
+const STATES = {
+  [TypeState.Active]: 'active',
+  [TypeState.Idle]: 'idle',
+  [TypeState.Paused]: 'paused',
+} as const
+
+const calculateUpdateStepsMax = (timestep: number) => Math.round(4000 / timestep)
+
+const createState = (
+  options: { type?: TypeState } & Partial<Options>,
+  subscriptions: SubscriptionState[] = [],
+) => {
+  assertOptions(options)
+
   // An exponential moving average of the frames per second.
-  const fps = 0
+  const frameDeltaEMA = 1000
 
   // The amount of time (in milliseconds) to simulate each time update()
   // runs
-  const timestep = options.timestep ?? 1000 / 60
+  const timestep = options.timestep ?? DEFAULT_TIMESTEP
 
   // The cumulative amount of in-app time that hasn't been simulated yet.
   const frameDelta = 0
 
   // The timestamp in milliseconds of the last time the main loop was run.
-  const lastFrameTime = 0
-
-  // A factor that affects how heavily to weight more recent seconds'
-  // performance when calculating the average frames per second. Valid values
-  // range from zero to one inclusive. Higher values result in weighting more
-  // recent seconds more heavily.
-  const fpsAlpha = 0.9
-
-  // The minimum duration between updates to the frames-per-second estimate.
-  // Higher values increase accuracy, but result in slower updates.
-  const fpsUpdateInterval = 1000
+  const timestampLast = 0
 
   // The timestamp (in milliseconds) of the last time the `fps` moving
   // average was updated.
-  const lastFpsUpdate = 0
+  const frameDeltaEMALastUpdate = 0
 
   // The number of frames delivered since the last time the `fps` moving
   // average was updated (i.e. since `lastFpsUpdate`).
-  const framesSinceLastFpsUpdate = 0
+  const frameDeltaEMAFramesSinceLastUpdate = 0
 
   // The number of times update() is called in a given frame. This is only
   // relevant inside of animate(), but a reference is held externally so that
   // this variable is not marked for garbage collection every time the main
   // loop runs.
-  const numberUpdateSteps = 0
-  const maxUpdateSteps = calculateMaxUpdateSteps(timestep)
+  const updateSteps = 0
+  const updateStepsMax = calculateUpdateStepsMax(timestep)
 
   // The minimum amount of time in milliseconds that must pass since the last
   // frame was executed before another frame can be executed.
-  const minDrawDelay = 1000 / (options.fps ?? Infinity)
+  const frameDeltaMin = 1000 / (options.fps ?? 60)
 
-  const lastDrawTime = 0
-
-  const isActive = false
+  const type = options.type ?? TypeState.Paused
 
   // Whether the simulation has fallen too far behind real time.
   // Specifically, `panic` will be set to `true` if too many updates occur in
@@ -101,71 +153,118 @@ const createState = (options: Partial<Pick<Options, 'fps' | 'timestep'>>) => {
 
   // The ID of the currently executing frame. Used to cancel frames when
   // stopping the loop.
-  const rafHandle = 0
+  const frameId = 0
+
+  const frameRequest: RequestAnimationFrame =
+    options.requestAnimationFrame ?? requestAnimationFrame.bind(window)
+
+  const frameCancel: CancelAnimationFrame =
+    options.cancelAnimationFrame ?? cancelAnimationFrame.bind(window)
 
   return {
-    fps,
-    fpsAlpha,
-    fpsUpdateInterval,
+    frameCancel,
     frameDelta,
-    framesSinceLastFpsUpdate,
-    isActive,
-    lastDrawTime,
-    lastFpsUpdate,
-    lastFrameTime,
-    maxUpdateSteps,
-    minDrawDelay,
-    numUpdateSteps: numberUpdateSteps,
+    frameDeltaEMA,
+    frameDeltaEMAFramesSinceLastUpdate,
+    frameDeltaEMALastUpdate,
+    frameDeltaMin,
+    frameId,
+    frameRequest,
     panic,
-    rafHandle,
+    subscriptions,
+    timestampLast,
     timestep,
+    type,
+    updateSteps,
+    updateStepsMax,
   }
 }
 
 export const snaproll = (options: Partial<Options> = {}) => {
   let state = createState(options)
 
-  const subscriptions = new Set<Subscription>()
-
   const multiplexer = (action: Action) => {
-    subscriptions.forEach((value) => value(action))
+    const reference = state.subscriptions
+    const length = reference.length
+
+    for (let index = 0; index < length; index++) {
+      const subscription = reference[index]
+
+      if (subscription.active) {
+        subscription.value(action)
+      }
+    }
   }
 
-  const _requestAnimationFrame: RequestAnimationFrame =
-    options.requestAnimationFrame ?? requestAnimationFrame
-
-  const _cancelAnimationFrame: CancelAnimationFrame =
-    options.cancelAnimationFrame ?? cancelAnimationFrame
-
   const resume = () => {
-    if (!state.isActive) {
-      state.isActive = true
+    if (state.type === TypeState.Active) {
+      return
+    }
 
-      state.rafHandle = _requestAnimationFrame((timestamp) => {
-        state.lastFrameTime = timestamp
-        state.lastFpsUpdate = timestamp
-        state.lastDrawTime = timestamp
-        state.framesSinceLastFpsUpdate = 0
+    state.type = subscriptionActive(state.subscriptions) ? TypeState.Active : TypeState.Idle
 
-        state.rafHandle = _requestAnimationFrame(animate)
+    if (state.type === TypeState.Active) {
+      state.frameId = state.frameRequest((timestamp) => {
+        state.timestampLast = timestamp
+        state.frameDeltaEMALastUpdate = timestamp
+        state.frameDeltaEMAFramesSinceLastUpdate = 0
+
+        state.frameId = state.frameRequest(animate)
       })
     }
   }
 
+  const idle = () => {
+    if (state.type !== TypeState.Active || subscriptionActive(state.subscriptions)) {
+      return
+    }
+
+    state.frameCancel(state.frameId)
+    state.type = TypeState.Idle
+  }
+
   const pause = () => {
-    state.isActive = false
-    _cancelAnimationFrame(state.rafHandle)
+    if (state.type === TypeState.Paused) {
+      return
+    }
+
+    if (state.type === TypeState.Active) {
+      state.frameCancel(state.frameId)
+    }
+
+    state.type = TypeState.Paused
   }
 
   const animate = (timestamp: number) => {
-    state.rafHandle = _requestAnimationFrame(animate)
+    state.frameId = state.frameRequest(animate)
+    const frameDelta = timestamp - state.timestampLast
+    const frameDeltaDeviation = Math.max(0, state.frameDeltaMin - frameDelta) / state.frameDeltaEMA
 
-    // if (state.lastFrameTime > timestamp) {
-    //   return
-    // }
+    if (state.timestampLast + frameDeltaDeviation * state.frameDeltaMin > timestamp) {
+      return
+    }
 
-    state.frameDelta += timestamp - state.lastFrameTime
-    state.lastFrameTime = timestamp
+    const frameDeltaEMAAlpha = 0.2
+    // TODO: which is more performant
+
+    // state.frameDeltaEMA =
+    //   1 / (frameDeltaEMAAlpha / (frameDelta) + (1 - frameDeltaEMAAlpha) / state.frameDeltaEMA)
+
+    const frameDeltaEMAUpdateInterval = 300
+    if (timestamp > state.frameDeltaEMALastUpdate + frameDeltaEMAUpdateInterval) {
+      state.frameDeltaEMA =
+        1 /
+        ((frameDeltaEMAAlpha * state.frameDeltaEMAFramesSinceLastUpdate) /
+          (timestamp - state.frameDeltaEMALastUpdate) +
+          (1 - frameDeltaEMAAlpha) / state.frameDeltaEMA)
+
+      state.frameDeltaEMALastUpdate = timestamp
+      state.frameDeltaEMAFramesSinceLastUpdate = 0
+    }
+    state.frameDeltaEMAFramesSinceLastUpdate++
+
+    state.frameDelta += frameDelta
+    state.timestampLast = timestamp
 
     multiplexer({
       frameDelta: state.frameDelta,
@@ -173,43 +272,23 @@ export const snaproll = (options: Partial<Options> = {}) => {
       type: TypeAction.FrameBegin,
     })
 
-    state.numUpdateSteps = 0
-
+    state.updateSteps = 0
     while (state.frameDelta >= state.timestep) {
       multiplexer({ timestep: state.timestep, type: TypeAction.FrameUpdate })
       state.frameDelta -= state.timestep
 
       // 4 seconds
-      if (++state.numUpdateSteps >= state.maxUpdateSteps) {
+      if (++state.updateSteps >= state.updateStepsMax) {
         state.panic = true
         break
       }
     }
 
-    const drawDelta = timestamp - state.lastDrawTime
-
-    if (drawDelta >= state.minDrawDelay) {
-      state.lastDrawTime =
-        timestamp - (state.minDrawDelay === 0 ? 0 : drawDelta % state.minDrawDelay)
-
-      if (timestamp > state.lastFpsUpdate + state.fpsUpdateInterval) {
-        state.fps =
-          (state.fpsAlpha * state.framesSinceLastFpsUpdate * 1000) /
-            (timestamp - state.lastFpsUpdate) +
-          (1 - state.fpsAlpha) * state.fps
-
-        state.lastFpsUpdate = timestamp
-        state.framesSinceLastFpsUpdate = 0
-      }
-
-      state.framesSinceLastFpsUpdate++
-
-      multiplexer({
-        delta: state.frameDelta / state.timestep,
-        panic: state.panic,
-        type: TypeAction.FrameDraw,
-      })
-    }
+    multiplexer({
+      delta: state.frameDelta / state.timestep,
+      panic: state.panic,
+      type: TypeAction.FrameDraw,
+    })
 
     // Run any updates that are not dependent on time in the simulation.
     multiplexer({
@@ -222,28 +301,29 @@ export const snaproll = (options: Partial<Options> = {}) => {
 
   return {
     get fps() {
-      return state.fps
+      // return 1000 / state.frameDeltaEMA
+      return state.frameDeltaEMA !== 0 ? 1000 / state.frameDeltaEMA : 0
     },
     set fps(value: number) {
-      if (value <= 0) {
-        pause()
-      } else {
-        // Dividing by Infinity returns zero.
-        state.minDrawDelay = 1000 / value
-      }
-    },
-    get isActive() {
-      return state.isActive
+      assertIsFPS(value)
+
+      state.frameDeltaMin = 1000 / value
     },
     pause,
-    reset(options: Partial<Pick<Options, 'fps' | 'timestep'>>) {
-      pause()
+    reset(options: { keepSubscriptions?: boolean } & Partial<Options> = {}) {
+      const keepSubscriptions = options?.keepSubscriptions !== false
 
       // preserve the options
-      state = createState({
-        fps: options.fps ?? 1000 / state.minDrawDelay,
-        timestep: options.timestep ?? state.timestep,
-      })
+      state = createState(
+        {
+          cancelAnimationFrame: options.cancelAnimationFrame ?? state.frameCancel,
+          fps: options.fps ?? state.frameDeltaEMA,
+          requestAnimationFrame: options.requestAnimationFrame ?? state.frameRequest,
+          timestep: options.timestep ?? state.timestep,
+          type: state.type,
+        },
+        keepSubscriptions ? [...state.subscriptions] : undefined,
+      )
     },
     resetFrameDelta() {
       const oldFrameDelta = state.frameDelta
@@ -255,11 +335,57 @@ export const snaproll = (options: Partial<Options> = {}) => {
       return oldFrameDelta
     },
     resume,
-    subscribe(value: Subscription): Unsubscribe {
-      subscriptions.add(value)
+    get state() {
+      return STATES[state.type]
+    },
+    subscribe(value: Subscription, options?: { activate?: boolean }): SubscriptionControls {
+      if (subscriptionFind(value, state.subscriptions) === undefined) {
+        const reference = state.subscriptions
+        const active = options?.activate !== false
+        state.subscriptions = [...reference, { active, value }]
 
-      return () => {
-        subscriptions.delete(value)
+        if (state.type === TypeState.Idle) {
+          resume()
+        }
+      }
+
+      return {
+        pause: () => {
+          const subscriptions = state.subscriptions
+          const subscription = subscriptionFind(value, subscriptions)
+
+          if (subscription === undefined) {
+            return
+          }
+
+          subscription.active = false
+          idle()
+        },
+        resume: () => {
+          const subscriptions = state.subscriptions
+          const subscription = subscriptionFind(value, subscriptions)
+
+          if (subscription === undefined) {
+            return
+          }
+
+          subscription.active = true
+
+          if (state.type === TypeState.Idle) {
+            resume()
+          }
+        },
+        unsubscribe() {
+          const subscriptions = state.subscriptions
+          const subscription = subscriptionFind(value, subscriptions)
+
+          if (subscription === undefined) {
+            return
+          }
+
+          state.subscriptions = subscriptionRemove(value, subscriptions)
+          idle()
+        },
       }
     },
     // How many milliseconds should be simulated by every update.
@@ -268,11 +394,10 @@ export const snaproll = (options: Partial<Options> = {}) => {
     },
     // How many milliseconds should be simulated by every update.
     set timestep(value: number) {
+      assertIsTimestep(value)
+
       state.timestep = value
-      state.maxUpdateSteps = calculateMaxUpdateSteps(value)
-    },
-    unsubscribe(value: Subscription) {
-      subscriptions.delete(value)
+      state.updateStepsMax = calculateUpdateStepsMax(state.timestep)
     },
   }
 }
