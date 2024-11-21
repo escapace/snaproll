d07e736ce202db15914d52ad473d5bd42de043a7
yyxi <178086254+yyxi@users.noreply.github.com>
2024-11-21 06:59:40 +0000

    chore: upkeep

--- src/esroll.d.ts
+++ src/esroll.d.ts
@@ -1,6 +1,6 @@
 declare global {
-  const __PLATFORM__: 'browser' | undefined
   const __ENVIRONMENT__: 'production' | 'staging' | 'testing' | undefined
+  const __PLATFORM__: 'browser' | undefined
   const __VERSION__: string | undefined
 }
 
